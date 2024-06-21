589ca0fc022fc4c46442f6ee81429580fb5a0333
EVK270599 <venkatakaustubha@gmail.com>
2024-06-21 10:12:35 -0700

    Week 7 Commits

--- lab-assignment7/cryptography-lab.c
+++ lab-assignment7/cryptography-lab.c
@@ -1,5 +1,5 @@
-//enter your name here
-//enter your email here
+// name: Kaustubha Eluri
+// email: eluri.k@northeastern.edu
 
 #include <stdio.h>
 #include <string.h>
@@ -8,35 +8,58 @@
 /* function to encrypt the data*/
 void encrypt(char text[], int key)
 {
-    // Add your code here
+    int i;
+    for(i = 0; i < strlen(text); i++)
+    {
+        // Check if the character is an uppercase letter
+        if(isupper(text[i]))
+        {
+            text[i] = (text[i] - 'A' + key) % 26 + 'A';
+        }
+        // Check if the character is a lowercase letter
+        else if(islower(text[i]))
+        {
+            text[i] = (text[i] - 'a' + key) % 26 + 'a';
+        }
+    }
 }
 
-/*function to decrypt the data*/
-void decrypt(char text[],int key)
+/* function to decrypt the data */
+void decrypt(char text[], int key)
 {
-    
-    // Add your code here
-    
+    int i;
+    for(i = 0; i < strlen(text); i++)
+    {
+        // Check if the character is an uppercase letter
+        if(isupper(text[i]))
+        {
+            text[i] = (text[i] - 'A' - key + 26) % 26 + 'A';
+        }
+        // Check if the character is a lowercase letter
+        else if(islower(text[i]))
+        {
+            text[i] = (text[i] - 'a' - key + 26) % 26 + 'a';
+        }
+    }
 }
-
 
 /*----------- Main program---------------*/
 int main()
 {
-    char text[20] ;
-    int keyvalue=3;
+    char text[20];
+    int keyvalue = 3;
     /*Input string */
     printf("Enter the message:  ");
-    scanf("%s",text);
-    printf("text input   = %s\n",text);
+    scanf("%s", text);
+    printf("Text input = %s\n", text);
     
     /*call function to encrypt the data*/
-    encrypt(text,keyvalue);
-    printf("Encrypted value = %s\n",text);
+    encrypt(text, keyvalue);
+    printf("Encrypted value = %s\n", text);
     
     /*call function to decrypt the data*/
-    decrypt(text,keyvalue);
-    printf("Decrypted value = %s\n",text);
+    decrypt(text, keyvalue);
+    printf("Decrypted value = %s\n", text);
     
     return 0;
 }
